1b07a9b0d8c404c2508c2de0a6d4f038a3f53f9a
Zedpai <70021545+Zedpaixd@users.noreply.github.com>
2020-09-15 02:35:07 +0300

    Update League2OP.gg1.py

--- League2OP.gg1.py
+++ League2OP.gg1.py
@@ -1,34 +1,44 @@
 from bs4 import BeautifulSoup
 import requests
 
+region = input("What region? (EUW/EUNE/NA/OCE/etc): ").lower()
 a = input()
 b = a.replace("joined the lobby",",")
 p = b.split(",")
 
-base = "https://euw.op.gg/summoner/userName="
-for i in range (0,5):
-    res = requests.get(base+p[i])
+print ("\n\n\n")
+
+base1 = "https://"
+base2 = ".op.gg/summoner/userName="
+
+for i in range (0,len(p)-1):
+    res = requests.get(base1+region+base2+p[i])
     link = res.text
     soup = BeautifulSoup(link,'lxml')
+
     elo1=soup.find('div',class_="SummonerRatingMedium")
     elo2=elo1.find('div',class_="TierRank")
     elo3=elo1.find('span',class_="LeaguePoints")
+    
     try:
         c=list(elo3)
     except:
         c=""
     elo4=elo1.find('span',class_="winratio")
     print ("Player name:",p[i])
+    
     try:
         print ("Rank:",list(elo2)[0].strip())
     except:
         pass
+    
     try:
         print (''.join(c).strip())
     except:
         pass
+    
     try:
-        print (list(elo4)[0].strip(),"\n\n")
+        print (list(elo4)[0].strip(),"\n")
     except:
         pass
 
