e1b422036be41bffdff1d02c3df658f8e39e9b58
Zedpai <70021545+Zedpaixd@users.noreply.github.com>
2020-09-16 03:02:15 +0300

    Update League2OP.gg1.py

--- League2OP.gg1.py
+++ League2OP.gg1.py
@@ -12,25 +12,39 @@ base1 = "https://"
 base2 = ".op.gg/summoner/userName="
 
 for i in range (0,len(p)-1):
+    
     res = requests.get(base1+region+base2+p[i])
     link = res.text
     soup = BeautifulSoup(link,'lxml')
 
     elo1=soup.find('div',class_="SummonerRatingMedium")
-    elo2=elo1.find('div',class_="TierRank")
-    elo3=elo1.find('span',class_="LeaguePoints")
+
+    try:
+        elo2=elo1.find('div',class_="TierRank")
+    except:
+        elo2 = ""
+
+    try:
+        elo3=elo1.find('span',class_="LeaguePoints")
+    except:
+        elo3 = ""
     
+    try:
+        elo4=elo1.find('span',class_="winratio")
+    except:
+        elo4 = ""
+
     try:
         c=list(elo3)
     except:
         c=""
-    elo4=elo1.find('span',class_="winratio")
+
     print ("Player name:",p[i])
     
     try:
         print ("Rank:",list(elo2)[0].strip())
     except:
-        pass
+        print ("Does not exist.")
     
     try:
         print (''.join(c).strip())
